48d48b793340bafd11011c5aae168d9f994df575
Ubuntu <ubuntu@ip-172-31-80-56.ec2.internal>
2020-04-15 14:52:24 +0000

    Time Entry Controller refactored

--- src/main/java/io.pivotal.pal.tracker/InMemoryTimeEntryRepository.java
+++ src/main/java/io.pivotal.pal.tracker/InMemoryTimeEntryRepository.java
@@ -6,41 +6,40 @@ import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
-@Component
+
 public class InMemoryTimeEntryRepository implements TimeEntryRepository{
     HashMap<Long, TimeEntry> inMemoryMap;
     long nextKey = 1;
     public InMemoryTimeEntryRepository() {
         this.inMemoryMap = new HashMap<>();
     }
-
+    @Override
     public TimeEntry create(TimeEntry timeEntry)   {
-
-        timeEntry.setId(nextKey);
-        this.inMemoryMap.put(nextKey, timeEntry);
+        TimeEntry newTimeEntry = new TimeEntry(nextKey, timeEntry.getProjectId(), timeEntry.getUserId(), timeEntry.getDate(),timeEntry.getHours());
+        this.inMemoryMap.put(nextKey, newTimeEntry);
             this.nextKey++;
-            return this.find(timeEntry.getId());
+            return this.find(newTimeEntry.getId());
     }
-
+    @Override
     public TimeEntry find(long id)  {
             return this.inMemoryMap.get(id);
     }
-
+    @Override
     public List<TimeEntry> list(){
             return new ArrayList<TimeEntry> (this.inMemoryMap.values());
     }
-
+    @Override
     public TimeEntry update(long id, TimeEntry timeEntry) {
-            timeEntry.setId(id);
-            if (this.inMemoryMap.containsKey(id)) {
-                this.inMemoryMap.put(id, timeEntry);
+        TimeEntry newTimeEntry = new TimeEntry(id, timeEntry.getProjectId(), timeEntry.getUserId(), timeEntry.getDate(),timeEntry.getHours());
+        if (this.inMemoryMap.containsKey(id)) {
+                this.inMemoryMap.replace(id, newTimeEntry);
                 return this.find(id);
             }
             else {
                 return null;
             }
     }
-
+    @Override
     public void delete(long id)  {
             this.inMemoryMap.remove(id);
     }
