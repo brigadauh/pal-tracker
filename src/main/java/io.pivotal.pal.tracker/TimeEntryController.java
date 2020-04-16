0b1ff3c4230119a5d41913f9412653866b0d4206
Ubuntu <ubuntu@ip-172-31-80-56.ec2.internal>
2020-04-16 15:48:25 +0000

    Health indicator

--- src/main/java/io.pivotal.pal.tracker/TimeEntryController.java
+++ src/main/java/io.pivotal.pal.tracker/TimeEntryController.java
@@ -1,5 +1,8 @@
 package io.pivotal.pal.tracker;
 
+import io.micrometer.core.instrument.Counter;
+import io.micrometer.core.instrument.DistributionSummary;
+import io.micrometer.core.instrument.MeterRegistry;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
@@ -10,13 +13,20 @@ import java.util.List;
 @RestController
 public class TimeEntryController {
     private TimeEntryRepository timeEntryRepository;
-    public TimeEntryController(TimeEntryRepository timeEntryRepository) {
+    private final DistributionSummary timeEntrySummary;
+    private final Counter actionCounter;
+    public TimeEntryController(TimeEntryRepository timeEntryRepository,
+                               MeterRegistry meterRegistry) {
         this.timeEntryRepository = timeEntryRepository;
+        timeEntrySummary = meterRegistry.summary("timeEntry.summary");
+        actionCounter = meterRegistry.counter("timeEntry.actionCounter");
     }
     @PostMapping("/time-entries")
     public ResponseEntity<TimeEntry> create(@RequestBody TimeEntry timeEntry) {
         try {
             TimeEntry result = this.timeEntryRepository.create(timeEntry);
+            actionCounter.increment();
+            timeEntrySummary.record(this.timeEntryRepository.list().size());
             return new ResponseEntity<>(result, HttpStatus.CREATED);
         }
         catch (Exception e) {
@@ -28,6 +38,7 @@ public class TimeEntryController {
         try {
             TimeEntry t = this.timeEntryRepository.find(timeEntryId);
             if (t != null) {
+                actionCounter.increment();
                 return new ResponseEntity<>(t, HttpStatus.OK);
             }
             else {
@@ -41,6 +52,7 @@ public class TimeEntryController {
     @GetMapping("/time-entries")
     public ResponseEntity<List<TimeEntry>> list() {
         try {
+            actionCounter.increment();
             return new ResponseEntity<>(this.timeEntryRepository.list(), HttpStatus.OK);
         }
         catch (Exception e) {
@@ -52,6 +64,7 @@ public class TimeEntryController {
         try {
             TimeEntry t = this.timeEntryRepository.update(id, expected);
             if (t != null) {
+                actionCounter.increment();
                 return new ResponseEntity<>(t, HttpStatus.OK);
             }
             else {
@@ -66,6 +79,8 @@ public class TimeEntryController {
     public ResponseEntity delete(@PathVariable("id") long id) {
         try {
             this.timeEntryRepository.delete(id);
+            actionCounter.increment();
+            timeEntrySummary.record(this.timeEntryRepository.list().size());
             return new ResponseEntity(HttpStatus.NO_CONTENT);
         }
         catch (Exception e) {
